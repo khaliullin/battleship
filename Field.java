44f2fd0024323d483d1ad1d3776e47069e0a749f
khaliullin <sgtkhaliullin@gmail.com>
2016-12-13 17:33:12 +0300

    Ship checking and adding to map made.

--- Field.java
+++ Field.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
  */
 public class Field {
     final byte SIZE = 10;
-    private byte [][] map = new byte[SIZE][SIZE];
+    private byte[][] map = new byte[SIZE][SIZE];
 
     private void initMap(byte[][] map) {
         for (int i = 0; i < SIZE; i++) {
@@ -18,19 +18,53 @@ public class Field {
     }
 
     @NotNull
-    private String addShip(Ship ship) {
-        String result = "";
+    private String checkShip(Ship ship) {
         for (Coordinates coordinates : ship.getCoordinates()) {
             byte x = coordinates.getX();
             byte y = coordinates.getY();
 
-            if (x > 10 || y > 10 || x < 0 || y < 0) {
-                result = "Index out of bounds";
-            }
-            else {
-                // TODO: 17.12.16 check with near ships
+            if (x > SIZE - 1 || y > SIZE - 1 || x < SIZE - 1 || y < SIZE - 1) {
+                return "Index out of bounds";
+            } else {
+                if (map[x][y] == 1) {
+                    return "This cell is busy";
+                }
+                // checking if ships near exist
+                else {
+                    if (x > 0) {
+                        if (map[x - 1][y] == 1) {
+                            return "Impossible to add ship there.";
+                        }
+                    }
+                    if (x < SIZE - 1) {
+                        if (map[x + 1][y] == 1) {
+                            return "Impossible to add ship there.";
+                        }
+                    }
+                    if (y > 0) {
+                        if (map[x][y - 1] == 1) {
+                            return "Impossible to add ship there.";
+                        }
+                    }
+                    if (y < SIZE - 1) {
+                        if (map[x][y + 1] == 1) {
+                            return "Impossible to add ship there.";
+                        }
+                    }
+                }
             }
         }
-         return result;
+        return "";
+    }
+
+    private boolean addShip(Ship ship) {
+        if (checkShip(ship).equals("")) {
+            for (Coordinates coordinates : ship.getCoordinates()) {
+                byte x = coordinates.getX();
+                byte y = coordinates.getY();
+                map[x][y] = 1;
+            }
+        }
+        return true;
     }
 }
