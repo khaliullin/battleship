56632c5d51ca78c338128f120a3b5def0e923b57
Damir <serazetdinow.damir@yandex.ru>
2016-12-20 12:58:04 +0300

    back buttons added

--- src/controllers/ConnectionController.java
+++ src/controllers/ConnectionController.java
@@ -1,6 +1,12 @@
 package controllers;
 
+import helpers.Helper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by 1 on 20.12.2016.
@@ -11,5 +17,14 @@ public class ConnectionController {
 
     public void setPrevStage(Stage prevStage) {
         this.prevStage = prevStage;
+    }
+
+    public void goToMainScreen(ActionEvent actionEvent) throws IOException {
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("../fxmls/mainScreen.fxml"));
+        Parent root = myLoader.load();
+        MainScreenController controller = myLoader.getController();
+        controller.setPrevStage(prevStage);
+
+        Helper.goTo(prevStage, "Connection", root);
     }
 }
--- src/controllers/roomCreationController.java
+++ src/controllers/roomCreationController.java
@@ -1,12 +1,17 @@
 package controllers;
 
 import classes.Field;
+import helpers.Helper;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,5 +35,14 @@ public class RoomCreationController implements Initializable{
     public void initialize(URL location, ResourceBundle resources) {
         paneField.getChildren().add(new Field());
         choiseBoxTime.getItems().addAll("0:30", "1:00", "1:30");
+    }
+
+    public void goToMainScreen(ActionEvent actionEvent) throws IOException {
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("../fxmls/mainScreen.fxml"));
+        Parent root = myLoader.load();
+        MainScreenController controller = myLoader.getController();
+        controller.setPrevStage(prevStage);
+
+        Helper.goTo(prevStage, "Connection", root);
     }
 }
