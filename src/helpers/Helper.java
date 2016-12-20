eb10d04f0d25e69527bd2151c5a2993483d98a80
Damir <serazetdinow.damir@yandex.ru>
2016-12-20 12:26:36 +0300

    helpers with goTo method added

---
+++ src/helpers/Helper.java
@@ -0,0 +1,17 @@
+package helpers;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by 1 on 20.12.2016.
+ */
+public class Helper {
+
+    public static void goTo(Stage prevStage, String title, Parent root){
+        prevStage.setTitle(title);
+        Scene scene = new Scene(root);
+        prevStage.setScene(scene);
+    }
+}
