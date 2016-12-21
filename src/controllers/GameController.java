426334568794a6bbf67b7ea521e2c336cfd807ad
Damir <serazetdinow.damir@yandex.ru>
2016-12-21 20:28:51 +0300

    initialize method with paneMyField and disabled paneEnemyField in GameController added.

--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -1,18 +1,31 @@
 package controllers;
 
+import classes.Field;
 import helpers.Helper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by 1 on 21.12.2016.
  */
-public class GameController {
+public class GameController implements Initializable{
+    public Label lblMyName;
+    public Label lblEnemyName;
+    public Label lblTime;
+    public Pane paneEnemyField;
+    public Pane paneMyField;
     private Stage prevStage;
+
+
 
     public void setPrevStage(Stage prevStage) {
         this.prevStage = prevStage;
@@ -26,5 +39,12 @@ public class GameController {
         controller.setPrevStage(prevStage);
 
         Helper.goTo(prevStage, "Main Screen", root);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        paneMyField.getChildren().add(new Field());
+        paneEnemyField.getChildren().add(new Field());
+        paneEnemyField.setDisable(true);
     }
 }
